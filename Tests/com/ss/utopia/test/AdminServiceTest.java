00f25650058bbe1c5dff8a3212cf4efa8f1ba275
IainLR <iainrobertzon@gmail.com>
2021-07-30 15:54:33 -0500

    updates

---
+++ Tests/com/ss/utopia/test/AdminServiceTest.java
@@ -0,0 +1,39 @@
+package com.ss.utopia.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+import com.ss.utopia.service.AdminService;
+
+public class AdminServiceTest {
+	AdminService adminService = new AdminService();
+
+	@Test
+	public void readRoutesTest() throws SQLException {
+		assertNotEquals(null, adminService.readRoutes());
+		assertEquals("DFW", adminService.readRoutes().get(0).getOriginId());
+	}
+
+	@Test
+	public void readAirplanesTest() throws SQLException {
+		assertNotEquals(null, adminService.readAirplanes());
+		assertEquals(new Integer(1), adminService.readAirplanes().get(0).getAirplaneTypeId());
+	}
+
+	@Test
+	public void readAirports() throws SQLException {
+		assertNotEquals(null, adminService.readAiports());
+		assertEquals("Acapulco", adminService.readAiports().get(0).getCity());
+	}
+
+	@Test
+	public void readAirplaneTypes() throws SQLException {
+		assertNotEquals(null, adminService.readAirplaneTypes());
+		assertEquals(new Integer(50), adminService.readAirplaneTypes().get(0).getMaxCapacity());
+	}
+
+}
--- src/main/java/com/ss/utopia/service/ConnectionUtil.java
+++ src/main/java/com/ss/utopia/service/ConnectionUtil.java
@@ -8,7 +8,6 @@ public class ConnectionUtil {
 	private String driver = "com.mysql.cj.jdbc.Driver";
 	private String url = "jdbc:mysql://localhost:3306/utopia";
 	private String username = "root";
-	private String password = "Kira-1701-D";
 
 	public Connection getConnection() throws SQLException, ClassNotFoundException {
 		// 1. Register driver
